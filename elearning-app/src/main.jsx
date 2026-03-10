e1a787ed80be17353a263ab5889398e547a32e2a
federico <ps3fga@gmail.com>
2026-03-10 18:32:27 -0300

    busqueda error supabase

--- elearning-app/src/main.jsx
+++ elearning-app/src/main.jsx
@@ -24,7 +24,7 @@ ReactDOM.createRoot(document.getElementById('root')).render(
       signUpUrl={`${BASE}sign-up`}
     >
       <SupabaseProvider>
-        <BrowserRouter basename="/">
+        <BrowserRouter basename={BASE}>
           <App />
         </BrowserRouter>
       </SupabaseProvider>
